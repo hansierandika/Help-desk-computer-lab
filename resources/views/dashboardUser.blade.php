b98d58774ba063e61b85888e71a4d1522c9d333b
Hansi Erandika <erandikahansi95@gmail.com>
2020-02-02 02:12:02 +0530

    userdashboard

--- resources/views/dashboardUser.blade.php
+++ resources/views/dashboardUser.blade.php
@@ -6,7 +6,7 @@ Registration
 
 @section('content')
 
-<div class="card" id="dash1" style="background-color:white;">
+<div class="card" id="dash1" style="background-color:#FBF7F9;">
         <div class="row">
                 <div class="col-md-5">
                         <h3 style="color: indigo;font-family: 'Aclonica';">Welcome {{ session()->get('username') }}</h3>
@@ -17,8 +17,8 @@ Registration
         </div>
         <div class="row">
         <div class="col-md-7">
-            <div class="container">
-                <div class="card" style="" id="contReport">
+            <div class="container" style="margin-left: 2%;background-color: #FBF7F9;">
+                <div class="card" style="background-color: #FBF7F9;" id="contReport">
 
                     @if(count($errors)>0)
                     <div class="alert alert-danger">
@@ -38,28 +38,38 @@ Registration
                     @endif
                     <form action="/insert" method="post" id="myform">
                         @csrf
-                         <fieldset>
+                         <fieldset class="form-group">
                         <legend>Help Desk Portal</legend>
                         <input type="hidden" name="id" value="{{ session()->get('id') }}" placeholder="00AAA0000">
                         <input type="hidden" name="name" value="{{ session()->get('username') }}" placeholder="00AAA0000">
 
-Computer Lab:
-<select name="ComputerLab">
-    <option name="ComputerLab" value="CIS Lab1">CIS Lab1</option>
-    <option name="ComputerLab" value="CIS Lab2">CIS Lab2</option>
-    <option name="ComputerLab" value="PST Lab">PST_lab</option>
-  </select>
-<br>
-                        <br>
-                        Machine Serial:
-                        <input type="text" name="machineSerial" id="machineSerial" placeholder="CIS01C05">
-                        <br>
-                        <br>
+                        <div class="form-group row">
+                            <label for="Computer_Lab" class="col-3 col-form-label">Computer Lab :</label>
+                            <div class="col-8">
+                                <select class="form-control form-control-sm" name="ComputerLab">
+                                    <option name="ComputerLab" value="CIS Lab1">CIS Lab1</option>
+                                    <option name="ComputerLab" value="CIS Lab2">CIS Lab2</option>
+                                    <option name="ComputerLab" value="PST Lab">PST_lab</option>
+                                </select>
+                             </div>
+                        </div>
 
-                        <input type="radio" class="btn btn-outline-primary" name="hardwareSoftware"  value="Hardware" id="bt1"/>hardware
-                        <input type="radio" class="btn btn-outline-primary" name="hardwareSoftware" value="Software" id="bt2"/>Software
 
-                                    <div class="row" style="display:none;" id="cont1">
+                            <div class="form-group row">
+                                <label for="Machine_Serial" class="col-3 col-form-label">Machine Serial :</label>
+                                <div class="col-8">
+                                    <input class="form-control form-control-sm" type="text" name="machineSerial" id="machineSerial" placeholder="CIS01C05">
+                                </div>
+                            </div>
+
+
+
+                            <label class="form-check-label">
+                        <input type="radio" class="btn btn-outline-primary" name="hardwareSoftware"  value="Hardware" id="bt1"/>hardware</label>
+                        <label class="form-check-label">
+                        <input type="radio" class="btn btn-outline-primary" name="hardwareSoftware" value="Software" id="bt2"/>Software</label>
+
+                                    <div class="row" style="display:none;margin-left: 2%" id="cont1">
                                         <div class="col-md-4">
                                             <input type="checkbox" name="type" value="keyboard">keyboard<br>
                                             <input type="checkbox" name="type" value="mouse">mouse<br>
@@ -73,7 +83,7 @@ Computer Lab:
                                         </div>
                                     </div>
                                     <div class="row" style="display:none;" id="cont2">
-                                        <textarea rows="3" cols="45" name="softwarediscription" style="margin-left: 10px" placeholder="Enter fault of software"></textarea>
+                                        <textarea rows="3" cols="45" name="softwarediscription" style="margin-left: 5%" placeholder="Enter fault of software"></textarea>
 
                                     </div>
 
